3b10663080b83568c924503cd8d45059667a6ea8
tadomikikuto-bit <lucien.programmer@gmail.com>
2020-09-16 22:48:53 +0300

    - fix: rename product od array except self func to solution

--- tests/test_prod_of_array_except_self.py
+++ tests/test_prod_of_array_except_self.py
@@ -3,7 +3,7 @@ from python_algo_solutions import product_of_array_except_self as poaes
 
 def test_should_return_prod_of_array_el_except_self():
 
-    assert poaes.product_of_array_except_self([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
-    assert poaes.product_of_array_except_self([3, 2, 1]) == [2, 3, 6]
-    assert poaes.product_of_array_except_self([1, 2, 3, 4]) == [24, 12, 8, 6]
+    assert poaes.solution([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
+    assert poaes.solution([3, 2, 1]) == [2, 3, 6]
+    assert poaes.solution([1, 2, 3, 4]) == [24, 12, 8, 6]
 
